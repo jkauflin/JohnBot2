6dee6b8a178b0241dbc09220fcc1c357ab38b14a
John J Kauflin <johnkauflin@gmail.com>
2019-10-05 14:34:06 -0400

    Getting localhost http working

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ Modification History
 
 // General handler for any uncaught exceptions
 process.on('uncaughtException', function (e) {
-  console.log("UncaughtException, error = " + e);
-  console.error(e.stack);
-  // Stop the process
-  // 2017-12-29 JJK - Don't stop for now, just log the error
-  //process.exit(1);
+    console.log("UncaughtException, error = " + e);
+    console.error(e.stack);
+    // Stop the process
+    // 2017-12-29 JJK - Don't stop for now, just log the error
+    //process.exit(1);
 });
 
 // Read environment variables from the .env file
@@ -84,31 +84,27 @@ var dateTime = require('node-datetime');
 
 // WebSocket URL to give to the client browser to establish ws connection
 var wsUrl;
-// Create a web server
-var express = require('express');
-var app = express();
-var httpServer;
-httpServer = new http.createServer(app)
-    .listen(process.env.HTTP_PORT, function () {
-        console.log("Live at Port " + process.env.HTTP_PORT + " - Let's rock!");
-    });
-
 var webServer;
 
 // If running local server just use HTTP, else use HTTPS
 if (process.env.HOST == "localhost") {
-  wsUrl = "ws://" + process.env.HOST + ":" + process.env.WEB_PORT;
+    wsUrl = "ws://" + process.env.HOST + ":" + process.env.WEB_PORT;
+    webServer = new http.createServer()
+        .listen(process.env.HTTP_PORT, function () {
+            console.log("HTTP WS server Live at Port " + process.env.WEB_PORT + " - Let's rock!");
+        });
+
+
 } else {
-  wsUrl = "wss://" + process.env.HOST + ":" + process.env.WEB_PORT;
-  //wsUrl = "wss://" + os.hostname + ":" + process.env.WEB_PORT;
-  webServer = new https.createServer({
-    // Key and certificate that have been signed by a CA root authority installed on server
-    key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_FILE_LOC),
-    cert: fs.readFileSync(process.env.SSL_PUBLIC_CERT_FILE_LOC)
-  })
-  .listen(process.env.WEB_PORT, function () {
-      console.log("WS server Live at Port " + process.env.WEB_PORT + " - Let's rock!");
-  });
+    wsUrl = "wss://" + process.env.HOST + ":" + process.env.WEB_PORT;
+    webServer = new https.createServer({
+        // Key and certificate that have been signed by a CA root authority installed on server
+        key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_FILE_LOC),
+        cert: fs.readFileSync(process.env.SSL_PUBLIC_CERT_FILE_LOC)
+    })
+        .listen(process.env.WEB_PORT, function () {
+            console.log("HTTPS WS server Live at Port " + process.env.WEB_PORT + " - Let's rock!");
+        });
 }
 
 //}, app)
@@ -116,31 +112,33 @@ if (process.env.HOST == "localhost") {
 //var dataFunctions = require('./dataFunctions.js');
 //var dataLoaded = false;
 //var audioFunctions = require('./audioFunctions.js');
-var botFunctions = require('./botFunctions.js');
+//var botFunctions = require('./botFunctions.js');
 
 //=================================================================================================
 // Create a WebSocket server and implement a heartbeat check
 //=================================================================================================
 const ws = require('ws');
+
 const webSocketServer = new ws.Server({ server: webServer, perMessageDeflate: false});
+
 // Initialize to false at the start
 ws.isAlive = false;
 function heartbeat() {
-  // If successful heartbeat call, set to true
-  this.isAlive = true;
+    // If successful heartbeat call, set to true
+    this.isAlive = true;
 }
 
 // Ping to monitor the websocket connection and terminate ws if no longer alive
 const interval = setInterval(function ping() {
-  webSocketServer.clients.forEach(function each(ws) {
-    //console.log(dateTime.create().format('Y-m-d H:M:S')+" In the ping, ws.readyState = "+ws.readyState);
-    if (ws.isAlive === false) {
-      return ws.terminate();
-    }
-    // Reset to false and request a ping (the pong response will set isAlive to true again)
-    ws.isAlive = false;
-    ws.ping('', false, true);
-  });
+    webSocketServer.clients.forEach(function each(ws) {
+        //console.log(dateTime.create().format('Y-m-d H:M:S')+" In the ping, ws.readyState = "+ws.readyState);
+        if (ws.isAlive === false) {
+            return ws.terminate();
+        }
+        // Reset to false and request a ping (the pong response will set isAlive to true again)
+        ws.isAlive = false;
+        ws.ping('', false, true);
+    });
 }, 30000);
 
 
@@ -148,58 +146,50 @@ const interval = setInterval(function ping() {
 // Successful connection from a web client
 //=================================================================================================
 webSocketServer.on('connection', function (ws) {
-  // Set to true after getting a successfuly connection from a web client
-  ws.isAlive = true;
-  // If you get a pong response from a client call the heartbeat function to set a variable
-  // showing the connection is still alive
-  ws.on('pong', heartbeat);
+    // Set to true after getting a successfuly connection from a web client
+    ws.isAlive = true;
+    // If you get a pong response from a client call the heartbeat function to set a variable
+    // showing the connection is still alive
+    ws.on('pong', heartbeat);
 
-  // General function to send a message from the bot to the connected browser client
-  function _wsSend(serverMessage) {
-    // Make sure the web socket is connected before trying to send the message
-    if (ws.isAlive == true) {
-      // JJK - you can either construct it as a string and send with no JSON.stringify
-      //       or construct a JSON object, with easier syntax, and then you have to stringify it
-      try {
-          ws.send(JSON.stringify(serverMessage));
-      } catch (error) {
-          // Ignore errors - just log to the console
-          console.error("Error in ws send to client, err = "+error);
-      }
-      
-    } else {
-      console.log("Error trying to send ws message = " + JSON.stringify(serverMessage));
+    // General function to send a message from the bot to the connected browser client
+    function _wsSend(serverMessage) {
+        // Make sure the web socket is connected before trying to send the message
+        if (ws.isAlive == true) {
+            // JJK - you can either construct it as a string and send with no JSON.stringify
+            //       or construct a JSON object, with easier syntax, and then you have to stringify it
+            try {
+                ws.send(JSON.stringify(serverMessage));
+            } catch (error) {
+                // Ignore errors - just log to the console
+                console.error("Error in ws send to client, err = " + error);
+            }
+
+        } else {
+            console.log("Error trying to send ws message = " + JSON.stringify(serverMessage));
+        }
     }
-  }
 
-  // Handle messages from the client browser
-  ws.on('message', function (botMessageStr) {
-    //console.log(dateTime.create().format('H:M:S.N') + ", botMessageStr = "+botMessageStr);
-    // Use JSON.parse to turn the string into a JSON object
-    var botMessage = JSON.parse(botMessageStr);
-    // Send the message object to the botFunctions module
-    botFunctions.command(botMessage);
-  });
+    // Handle messages from the client browser
+    ws.on('message', function (botMessageStr) {
+        //console.log(dateTime.create().format('H:M:S.N') + ", botMessageStr = "+botMessageStr);
+        // Use JSON.parse to turn the string into a JSON object
+        var botMessage = JSON.parse(botMessageStr);
+        // Send the message object to the botFunctions module
+        botFunctions.command(botMessage);
+    });
 
-  // Register event listeners for the bot events
-  botFunctions.botEvent.on("error", function (errorMessage) {
-    _wsSend({ "errorMessage": errorMessage });
-  });
+    // Register event listeners for the bot events
+    botFunctions.botEvent.on("error", function (errorMessage) {
+        _wsSend({ "errorMessage": errorMessage });
+    });
 
-  botFunctions.botEvent.on("proxIn", function (proxIn) {
-    _wsSend({ "proxIn": proxIn });
-  });
+    botFunctions.botEvent.on("proxIn", function (proxIn) {
+        _wsSend({ "proxIn": proxIn });
+    });
 
   // When initiating speech from the robot, send to browser client to speak
   // and to robot to animate
   //?????
   // Hey, I've hit a wall - someone help me
-  /*
-  function sayAndAnimate(textToSpeak) {
-    _wsSend({ "textToSpeak": textToSpeak });
-    // Animate the bot when speaking
-    botFunctions.animateSpeech(textToSpeak);
-  }
-  */
 }); // End of Connection to client
-
