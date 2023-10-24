2e16484e353175fd5d552e95d698fcfac90d2c57
John J Kauflin <johnkauflin@gmail.com>
2023-10-24 17:27:03 -0400

    test

--- server.mjs
+++ server.mjs
@@ -76,9 +76,12 @@ import {log} from './util.mjs'
 
 const fastify = Fastify({
     logger: true,
-    // Key and certificate that have been signed by a CA root authority installed on server
-    key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_FILE_LOC),
-    cert: fs.readFileSync(process.env.SSL_PUBLIC_CERT_FILE_LOC)
+    http2: true,
+    https: {
+      // Key and certificate that have been signed by a CA root authority installed on server
+      key: fs.readFileSync(process.env.SSL_PRIVATE_KEY_FILE_LOC),
+      cert: fs.readFileSync(process.env.SSL_PUBLIC_CERT_FILE_LOC)
+    }
 })
 
 const {Board,Led,Relays} = johnnyFivePkg
